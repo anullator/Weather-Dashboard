61da2f45321be630c1954210e32cb353db149460
anullator <danyelle.joy@gmail.com>
2024-04-09 18:20:18 -0500

    get, set, and render location history

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,33 +7,62 @@ const weatherAPI = "596fd2b39a8f6c88b0462b3b20ca7ef3"; //BC-weather-dashboard
 // ---------- CITY SEARCH ------------
 
 function handleCitySearch(event) {
+    const city = $('#city').val().trim();
+    $('#city').val('');
+    console.log(city);
     event.preventDefault();
+
 
     if (!city) {
         alert('Must enter city name');
     } else {
+        setLocationHistory();
         renderLocationHistory();
-        renderCurrWeather();
-        renderForecastCards();
+        renderWeather();
     }
 }
 
 function getLocationHistory() {
-    // const cities = [];
-    // if (localStorage.length !== 0) {
-    //     cities = localStorage.parse("cities");
-    // }
-    // const city = $('#city').val().trim();
-    // // alerts user until a city is entered
+    let cities = [];
 
-    //     //store city input to local storage aka the history
-        
-    //     localStorage.setItem("cities", city);
+    if (localStorage.length !== 0) {
+        cities = JSON.parse(localStorage.getItem('cities'));
+    }
+    return cities;
+}
+
+function setLocationHistory() {
+    let currCities = getLocationHistory();
+    const city = $('#city').val().trim();
+    if (localStorage.length !== 0) {
+        currCities = JSON.parse(localStorage.getItem('cities'));
+    } 
+    if (city && city !== currCities[0]) {
+        currCities.unshift(city); // add new city to history
+
+        //store city input to local storage aka the history
+        localStorage.setItem('cities', JSON.stringify(currCities));
+    }
+    return currCities;
 }
 
 // TODO:
 function renderLocationHistory() {
     console.log(`enter render location history`);
+    console.log(localStorage.length);
+    
+    $('#history').empty();
+
+    const history = getLocationHistory();
+    history.forEach(city => {
+        const btnEl = document.createElement('button');
+        btnEl.textContent = city;
+        
+        $('#history').append(btnEl);
+
+    })
+
+    $('#history > button').on('click', renderWeather);
 }
 
 // ------------ CURRENT WEATHER -------------
@@ -67,21 +96,26 @@ function renderForecastCards() {
         // add '.card' class to container
 }
 
+// -------- RENDER ALL WEATHER -----------
+
+function renderWeather() {
+    console.log(`enter render weather`);
+    renderCurrWeather();
+    renderForecastCards();
+}
+
 // --------------- ON LOAD ---------------
 
 $(document).ready(function () {
-        console.log(`enter document ready function`);
+    console.log(`enter document ready function`);
 
-        // render location history
-        renderLocationHistory();
+    // render location history
+    renderLocationHistory();
 
-        // add event listener to button
-        $("[type='submit']").on('click', handleCitySearch);
-    
-        // load current city weather
-        renderCurrWeather();
+    // load weather
+    renderWeather();
 
-        // load 5-day forecast
-        renderForecastCards();
-    });
+    // add event listener to button
+    $("[type='submit']").on('click', handleCitySearch);
+});
 
