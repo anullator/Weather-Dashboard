b50824b177aa0c2d1f93e4619880435e4ccec81a
anullator <danyelle.joy@gmail.com>
2024-04-12 00:07:57 -0500

    changed http to https for api request

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,7 @@ const weatherAPIKey = "596fd2b39a8f6c88b0462b3b20ca7ef3"; //BC-weather-dashboard
 const baseCurrWeather = `https://api.openweathermap.org/data/2.5/weather?`;
 
 // Geocoder API call
-const baseCoords = `http://api.openweathermap.org/geo/1.0/direct?q=`;
+const baseCoords = `https://api.openweathermap.org/geo/1.0/direct?q=`;
 
 // ---------- CITY SEARCH ------------
 
